36f738d5794cbfbd6ce5678474b7c0cd4af21a01
Dima Ovcharenko <DOvcharenko@korusconsulting.ru>
2019-12-09 23:40:16 +0300

    реализовал сохранение PID запускаемых процессов и их завершение

--- env.bat
+++ env.bat
@@ -1,9 +1,8 @@
 set 1COVERAGE_DBGS_HOST=localhost
 set 1COVERAGE_DBGS_PORT=1550
 
-set PROXY_RESULT_DIR=./build/log/dbgs.log
 set PROXY_PORT=3000
-set PROXY_URL=http://localhost:3000
+set PROXY_RESULT_DIR=./build/1coverage-dbgs-proxy.log
 
 set IBConnectionString=File=;
 set v8path="C:/Program Files/1cv8/_._.__.____/bin/"
